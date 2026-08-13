c2ce221cf507a815b503c4c7bfbba8a614f063c0
ValtenesJr25 <valtenesjunior25@gmail.com>
2026-08-13 15:18:02 -0300

    Update exercicio8.php

--- exercicio8.php
+++ exercicio8.php
@@ -1,1 +1,21 @@
+<?php
 
+function ehpar($numero){
+
+	if($numero %2 == 0){
+
+		return "par";
+
+	} else {
+
+		return "impar";
+
+	}
+
+}
+
+$numero = readline("Digite seu numero: ");
+
+print ehpar($numero);
+
+?>
